55c4658248ca8956cc3f81d47ea683647a46c3c7
Anton Lundin <anton@dohi.se>
2012-10-16 11:09:04 +0200

    Add some site theming

--- payload/30-setup-ckan-conf.sh
+++ payload/30-setup-ckan-conf.sh
@@ -11,3 +11,7 @@ chown www-data . data sstore
 
 # Config hostname
 perl -pi -e "s,^ckan.site_url.*$,ckan.site_url = http://$CKAN_HOSTNAME/," $CKAN_CFG
+
+# XXX: Hardcoded "theme"
+perl -pi -e 's/^ckan.site_title.*$/ckan.site_title = Openumea CKAN Site/' $CKAN_CFG
+perl -pi -e 's,^ckan.site_logo.*$,ckan.site_logo = http://www.umea.se/images/18.73d525c912cecb285058000245/umea-kommun-vit-logo.gif,' $CKAN_CFG
