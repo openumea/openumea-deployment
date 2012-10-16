e891d65cb88b5da360d152829369f79167b28929
Anton Lundin <anton@dohi.se>
2012-10-16 11:09:15 +0200

    Add config and hadling of s3 storage

--- payload/30-setup-ckan-conf.sh
+++ payload/30-setup-ckan-conf.sh
@@ -15,3 +15,9 @@ perl -pi -e "s,^ckan.site_url.*$,ckan.site_url = http://$CKAN_HOSTNAME/," $CKAN_
 # XXX: Hardcoded "theme"
 perl -pi -e 's/^ckan.site_title.*$/ckan.site_title = Openumea CKAN Site/' $CKAN_CFG
 perl -pi -e 's,^ckan.site_logo.*$,ckan.site_logo = http://www.umea.se/images/18.73d525c912cecb285058000245/umea-kommun-vit-logo.gif,' $CKAN_CFG
+
+# should we use s3-storage?
+if [ ! -z "$CKAN_STORAGE_BUCKET" ] ; then
+	perl -pi -e 's/^.*ofs.impl = s3.*$/ofs.impl = s3/' $CKAN_CFG
+	perl -pi -e "s/^.*ckan.storage.bucket.*$/ckan.storage.bucket = $CKAN_STORAGE_BUCKET/" $CKAN_CFG
+fi
