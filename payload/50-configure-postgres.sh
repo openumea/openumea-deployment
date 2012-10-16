5d7a4c63c4539f044c63f306775e1f563f739391
Anton Lundin <anton@dohi.se>
2012-10-16 11:38:27 +0200

    perl fooled away the @localhost, @ is lists in perl

--- payload/50-configure-postgres.sh
+++ payload/50-configure-postgres.sh
@@ -7,4 +7,4 @@ PW=$(pwgen 12 1)
 sudo -u postgres psql -c "CREATE ROLE ckan PASSWORD '$PW' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;"
 sudo -u postgres psql -c "CREATE DATABASE ckan OWNER ckan;"
 
-perl -pi -e "s,^sqlalchemy.url.*,sqlalchemy.url=postgresql://ckan:$PW@localhost/ckan?sslmode=disable,"  $CKAN_CFG
+perl -pi -e "s,^sqlalchemy.url.*,sqlalchemy.url=postgresql://ckan:$PW\@localhost/ckan?sslmode=disable,"  $CKAN_CFG
