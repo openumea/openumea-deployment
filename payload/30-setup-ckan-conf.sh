0c40da44989cc9d2d7a0d1fc36af78f20a11c0d6
Anton Lundin <anton@dohi.se>
2012-10-16 11:06:41 +0200

    Config mandotory ckan hostname

--- payload/30-setup-ckan-conf.sh
+++ payload/30-setup-ckan-conf.sh
@@ -8,3 +8,6 @@ $CKAN_VENV/bin/paster make-config ckan $CKAN_CFG
 ln -s $CKAN_VENV/src/ckan/who.ini
 mkdir data sstore
 chown www-data . data sstore
+
+# Config hostname
+perl -pi -e "s/^ckan.site_url.*$/ckan.site_url = $CKAN_HOSTNAME/" $CKAN_CFG
