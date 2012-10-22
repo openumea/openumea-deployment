e92b466ec365d1349712432fedae4ad4947a2484
Anton Lundin <anton@dohi.se>
2012-10-22 10:58:09 +0200

    Download, uncompress and load db-dumps

--- payload/60-restore-create-database.sh
+++ payload/60-restore-create-database.sh
@@ -3,8 +3,21 @@
 
 # Should we restore db from backup?
 if [ ! -z "$CKAN_BACKUP" ] ; then
-	# XXX: download backup?
+	# download backup?
+	if [ ! -e "$CKAN_BACKUP" ] ; then
+		CKAN_BACKUP_DOWNLOADED=$(basename $CKAN_BACKUP)
+		$CKAN_VENV/bin/fetch_file --out-file=$CKAN_BACKUP_DOWNLOADED $CKAN_BACKUP
+		CKAN_BACKUP=$CKAN_BACKUP_DOWNLOADED
+	fi
+	# uncompress
+	if [[ "$CKAN_BACKUP" = *".gz" ]] ; then
+		gunzip $CKAN_BACKUP
+		CKAN_BACKUP=$(echo $CKAN_BACKUP | sed -ie 's/\.gz$//')
+	fi
+	# load db dump
 	$CKAN_VENV/bin/paster --plugin=ckan db load --file=$CKAN_BACKUP --config=$CKAN_CFG
+	# And upgrade any changes
+	$CKAN_VENV/bin/paster --plugin=ckan db upgrade --config=$CKAN_CFG
 else
 	# init a fesh db
 	$CKAN_VENV/bin/paster --plugin=ckan db init --config=$CKAN_CFG
