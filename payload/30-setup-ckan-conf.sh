5bbf262cf0feadd0cf3126c64e50e738227b8667
Anton Lundin <anton@dohi.se>
2012-10-24 09:20:23 +0200

    Abstract out "theming" to separate script

--- payload/30-setup-ckan-conf.sh
+++ payload/30-setup-ckan-conf.sh
@@ -13,10 +13,6 @@ chown www-data . data sstore ckan.log
 # Config hostname
 perl -pi -e "s,^ckan.site_url.*$,ckan.site_url = http://$CKAN_HOSTNAME/," $CKAN_CFG
 
-# XXX: Hardcoded "theme"
-perl -pi -e 's/^ckan.site_title.*$/ckan.site_title = Openumea CKAN Site/' $CKAN_CFG
-perl -pi -e 's,^ckan.site_logo.*$,ckan.site_logo = http://www.umea.se/images/18.73d525c912cecb285058000245/umea-kommun-vit-logo.gif,' $CKAN_CFG
-
 # should we use s3-storage?
 if [ ! -z "$CKAN_STORAGE_BUCKET" ] ; then
 	perl -pi -e 's/^.*ofs.impl = s3.*$/ofs.impl = s3/' $CKAN_CFG
---
+++ payload/35-theme-ckan.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. $(dirname $0)/config
+
+# XXX: Hardcoded "theme"
+#perl -pi -e 's/^ckan.site_title.*$/ckan.site_title = /' $CKAN_CFG
+#perl -pi -e 's/^ckan.site_description.*$/ckan.site_description = /' $CKAN_CFG
+#perl -pi -e 's,^ckan.site_logo.*$,ckan.site_logo = ,' $CKAN_CFG
