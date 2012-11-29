5b723ad034da8a0fe502831016072a6e1b1a60c0
Anton Lundin <anton@dohi.se>
2012-11-29 14:55:36 +0100

    Automatic theming

--- payload/35-theme-ckan.sh
+++ payload/35-theme-ckan.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 . $(dirname $0)/config
 
+if [ "$CKAN_THEME" = "" ] ; then
+	exit 0;
+fi
+
 # XXX: Hardcoded "theme"
-#perl -pi -e 's/^ckan.site_title.*$/ckan.site_title = /' $CKAN_CFG
-#perl -pi -e 's/^ckan.site_description.*$/ckan.site_description = /' $CKAN_CFG
-#perl -pi -e 's,^ckan.site_logo.*$,ckan.site_logo = ,' $CKAN_CFG
+if [ "$CKAN_THEME" = "openumea" ] ; then
+	git clone --branch master git://github.com/openumea/openumea-theme.git $CKAN_ROOT/$CKAN_THEME
+
+	# Configure us to use title and logos
+	perl -pi -e 's/^ckan.site_title.*$/ckan.site_title = OpenUmea/' $CKAN_CFG
+	perl -pi -e 's/^ckan.site_description.*$/ckan.site_description = Open data from the municipality of Umea/' $CKAN_CFG
+
+	perl -pi -e 's|.*ckan.favicon =.*$|ckan.favicon = /images/icons/UmeaKommun.ico|' $CKAN_CFG
+	perl -pi -e 's|.*ckan.site_logo =.*$|ckan.site_logo = /images/UmeaKommun.png|' $CKAN_CFG
+fi
+
+# to make CKAN_THEME available in perl
+export CKAN_THEME
+
+# Enable themes public and templates dirs
+perl -pi -e 's|.*extra_template_paths =.*$|extra_template_paths = %(here)s/$ENV{"CKAN_THEME"}/templates|' $CKAN_CFG
+perl -pi -e 's|.*extra_public_paths =.*$|extra_public_paths = %(here)s/$ENV{"CKAN_THEME"}/public|' $CKAN_CFG
