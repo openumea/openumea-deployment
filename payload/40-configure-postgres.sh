62cdca69a04f76e3f4537a29f8fca64dccc111d0
Anton Lundin <anton@dohi.se>
2013-12-03 12:13:39 +0100

    Abstract out db user, name and host.

--- payload/40-configure-postgres.sh
+++ payload/40-configure-postgres.sh
@@ -3,8 +3,11 @@
 
 cd $CKAN_ROOT
 
-PW=$(pwgen 12 1)
-sudo -u postgres psql -c "CREATE ROLE ckan PASSWORD '$PW' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;"
-sudo -u postgres psql -c "CREATE DATABASE ckan OWNER ckan TEMPLATE template0 ENCODING 'UTF8';"
+CKAN_DB_USER=ckan
+CKAN_DB_PW=$(pwgen 12 1)
+CKAN_DB=ckan
+CKAN_DB_HOST=localhost
+sudo -u postgres psql -c "CREATE ROLE ${CKAN_DB_USER} PASSWORD '${CKAN_DB_PW}' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;"
+sudo -u postgres psql -c "CREATE DATABASE ${CKAN_DB} OWNER ${CKAN_DB_USER} TEMPLATE template0 ENCODING 'UTF8';"
 
-perl -pi -e "s,^sqlalchemy.url.*,sqlalchemy.url=postgresql://ckan:$PW\@localhost/ckan?sslmode=disable,"  $CKAN_CFG
+perl -pi -e "s,^sqlalchemy.url.*,sqlalchemy.url=postgresql://${CKAN_DB_USER}:${CKAN_DB_PW}\@${CKAN_DB_HOST}/${CKAN_DB}?sslmode=disable," $CKAN_CFG
