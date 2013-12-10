35b8dbe0e35851ded254e54f13c56837ce699bd7
Anton Lundin <anton@dohi.se>
2013-12-10 16:23:02 +0100

    Add support for restoring datastore backups

--- payload/60-restore-create-database.sh
+++ payload/60-restore-create-database.sh
@@ -29,3 +29,19 @@ else
 	$CKAN_VENV/bin/paster --plugin=ckan user add $CKAN_SYSADMIN_USER password="$CKAN_SYSADMIN_PW" email="$RECIPENT_EMAIL_ADDRESS" --config=$CKAN_CFG
 	$CKAN_VENV/bin/paster --plugin=ckan sysadmin add $CKAN_SYSADMIN_USER --config=$CKAN_CFG
 fi
+
+if [ "$DATASTORE" = "1" ] && [ ! -z "$DATASTORE_BACKUP" ] ; then
+	# download backup?
+	if [ ! -e "$DATASTORE_BACKUP" ] ; then
+		DATASTORE_BACKUP_DOWNLOADED=$CLOUD_DATA/$(basename $DATASTORE_BACKUP)
+		$CKAN_VENV/bin/fetch_file --out-file=$DATASTORE_BACKUP_DOWNLOADED $DATASTORE_BACKUP
+		DATASTORE_BACKUP=$DATASTORE_BACKUP_DOWNLOADED
+	fi
+	# uncompress
+	if [[ "$DATASTORE_BACKUP" = *".gz" ]] ; then
+		gunzip $DATASTORE_BACKUP
+		DATASTORE_BACKUP=$(echo $DATASTORE_BACKUP | sed -e 's/\.gz$//')
+	fi
+	# load db dump
+	sudo -u postgres psql datastore -f $DATASTORE_BACKUP
+fi
