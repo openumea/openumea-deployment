efcf0bf26c3e6e7d19abd60be2e438625f6040b3
Anton Lundin <anton@dohi.se>
2014-09-12 11:12:55 +0200

    Switch github urls from okfn to ckan
    Okfn have transitioned ckan repos to a "ckan" github organization. There
    are redirects in place for okfn/ckan -> ckan/ckan but this updates so we
    clone from the right url to begin with.

--- payload/20-install-source.sh
+++ payload/20-install-source.sh
@@ -3,7 +3,7 @@
 
 virtualenv $CKAN_VENV
 # Grab the release tag from github
-$CKAN_VENV/bin/pip install --ignore-installed -e git+git://github.com/okfn/ckan.git@ckan-2.1.2#egg=ckan
+$CKAN_VENV/bin/pip install --ignore-installed -e git+git://github.com/ckan/ckan.git@ckan-2.1.2#egg=ckan
 # install its dependencies
 $CKAN_VENV/bin/pip install --ignore-installed -r $CKAN_VENV/src/ckan/requirements.txt
 # install a good version of boto.
@@ -13,5 +13,5 @@ $CKAN_VENV/bin/pip install boto==2.20.1
 #$CKAN_VENV/bin/pip install celery
 
 # Example on how to install plugins
-#$CKAN_VENV/bin/pip install -e git+git://github.com/okfn/ckanext-datastorer.git#egg=ckanext-datastorer
+#$CKAN_VENV/bin/pip install -e git+git://github.com/ckan/ckanext-datastorer.git#egg=ckanext-datastorer
 #$CKAN_VENV/bin/pip install -r $CKAN_VENV/src/ckanext-datastorer/pip-requirements.txt
